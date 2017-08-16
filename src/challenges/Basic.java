8caf7423314f51907a70551012bfd72a88f6a7ff
David Szilagyi <szilagyidav@gmail.com>
2017-08-16 13:52:46 +0200

    Title Case a Sentence implemented

--- src/challenges/Basic.java
+++ src/challenges/Basic.java
@@ -44,4 +44,13 @@ public class Basic {
         }
         return longest;
     }
+
+    public String titleCase(String text) {
+        String[] words = text.split(" ");
+        String result = "";
+        for(String word : words) {
+            result += word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase() + " ";
+        }
+        return result;
+    }
 }
